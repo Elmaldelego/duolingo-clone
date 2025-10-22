ac5a8ceb009fffb5c28858a5a52243f0e4fd1c5e
Elmaldelego <127124164+Elmaldelego@users.noreply.github.com>
2025-10-22 16:31:07 -0600

    Update admin.ts

--- lib/admin.ts
+++ lib/admin.ts
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 
 export const getIsAdmin = async () => {
   const { userId } = await auth();
-  const adminIds = process.env.CLERK_ADMIN_IDS.split(", "); // stored in .env.local file as string separated by comma(,) and space( )
+  const adminIds = ["user_34QWdNsJSZZ3QEr7g5lb9yljWBT"];// stored in .env.local file as string separated by comma(,) and space( )
 
   if (!userId) return false;
 
