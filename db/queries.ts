6a74946ea10f1a9d765d009e8f965b98c29a778d
Elmaldelego <127124164+Elmaldelego@users.noreply.github.com>
2025-10-27 08:19:20 -0600

    Add files via upload

--- db/queries.ts
+++ db/queries.ts
@@ -22,21 +22,6 @@ export const getCourses = cache(async () => {
   return data;
 });
 
-export const getUserProgress = cache(async () => {
-  const { userId } = await auth();
-
-  if (!userId) return null;
-
-  const data = await db.query.userProgress.findFirst({
-    where: eq(userProgress.userId, userId),
-    with: {
-      activeCourse: true,
-    },
-  });
-
-  return data;
-});
-
 export const getUnits = cache(async () => {
   const { userId } = await auth();
   const userProgress = await getUserProgress();
@@ -98,11 +83,16 @@ export const getCourseById = cache(async (courseId: number) => {
   const data = await db.query.courses.findFirst({
     where: eq(courses.id, courseId),
     with: {
-      units: {
-        orderBy: (units, { asc }) => [asc(units.order)],
+      capitulos: {
+        orderBy: (capitulos, { asc }) => [asc(capitulos.order)],
         with: {
-          lessons: {
-            orderBy: (lessons, { asc }) => [asc(lessons.order)],
+          units: {
+            orderBy: (units, { asc }) => [asc(units.order)],
+            with: {
+              lessons: {
+                orderBy: (lessons, { asc }) => [asc(lessons.order)],
+              },
+            },
           },
         },
       },
